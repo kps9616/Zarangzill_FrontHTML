482eb3f2a956a0eb92478a8e93a01a5484ce99d6
bragging <kps9616@gmail.com>
2023-12-24 18:54:18 +0900

    Initial commit

---
+++ src/main/java/com/example/zaranzill_fronthtml/ZaranzillFrontHtmlApplication.java
@@ -0,0 +1,13 @@
+package com.example.zaranzill_fronthtml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaranzillFrontHtmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZaranzillFrontHtmlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zaranzill_fronthtml/ZaranzillFrontHtmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zaranzill_fronthtml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaranzillFrontHtmlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

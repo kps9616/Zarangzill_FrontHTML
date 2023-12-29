84c6d8c18d47e06d8e4ad85627b093c4a3b6d75a
bragging <kps9616@gmail.com>
2023-12-29 22:18:32 +0900

    ajax 용 js 추가

---
+++ src/main/resources/static/js/ajax.js
@@ -0,0 +1,27 @@
+function fn_get(form,apiUrl) {
+    $.ajax({
+        url: 'http://1.226.83.35:9090/api/v1/'+apiUrl,
+        method: 'GET',
+        data: $("#"+form).serialize(),
+        success: function(response) {
+            console.log(response);
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
+}
+
+function fn_post(form,apiUrl) {
+    $.ajax({
+        url: 'http://1.226.83.35:9090/api/v1/'+apiUrl,
+        method: 'POST',
+        data: $("#"+form).serialize(),
+        success: function(response) {
+            console.log(response);
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
+}
